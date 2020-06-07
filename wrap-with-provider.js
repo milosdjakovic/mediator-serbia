c7962272ee52c35274dda52f38a27d2ddff8fd22
Milos Djakovic <milos.g.djakovic@gmail.com>
2020-06-08 00:20:21 +0200

    File structure changes

--- wrap-with-provider.js
+++ wrap-with-provider.js
@@ -1,18 +1,12 @@
 import React from "react"
 import { Provider } from "react-redux"
 
-import store from "../store/store"
+const store = './src/store/store'
 
 // eslint-disable-next-line react/display-name,react/prop-types
-const WrapWithProvider = ({ element }) => {
+export default ({ element }) => {
   // Instantiating store in `wrapRootElement` handler ensures:
   //  - there is fresh store for each SSR page
   //  - it will be called only once in browser, when React mounts
-  return (
-    <Provider store={store}>
-      {element}
-    </Provider>
-  )
+  return <Provider store={store}>{element}</Provider>
 }
-
-export default WrapWithProvider
